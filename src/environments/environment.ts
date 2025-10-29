839dd36be8b13b6878ff5f5526b9e9d8a723e4df
DOC.WOW Restaurant <contact@docwow.de>
2025-10-29 12:11:08 +0100

    Build et config Netlify

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "ta_clé_firebase",
+    apiKey: "AIzaSyBY571lmuW24qnczKhCGORAGWg4gei8cek",
     authDomain: "afroconnect-a53a5.firebaseapp.com",
     projectId: "afroconnect-a53a5",
     storageBucket: "afroconnect-a53a5.appspot.com",
     messagingSenderId: "341889512681",
     appId: "1:341889512681:web:e4073a27dded8eae9e2c78"
   },
-  googleMapsApiKey: "ta_clé_maps"
+  googleMapsApiKey: "AIzaSyAVTCyd8uLieVgnMHEygb5mm1xQKcjiOVk"
 };
